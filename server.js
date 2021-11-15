4d4a4c67503cde345a27f588463fed219f4640d6
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-11-15 11:42:10 -0600

    fixing file structure mistake

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // const path = require('path');
 const express = require('express');
+const routes = require('./controllers/');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,7 @@ const sequelize = require('./config/connection');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(routes);
 
 // sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Listening on ${PORT}!`));
