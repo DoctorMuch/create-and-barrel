9e395672b9c67fdfb6e014769f37fc70abd5c8ac
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-11-18 14:40:55 -0600

    Add bottle button on dash adds to database. Needs to capture user id. Work in progress

--- public/javascript/add-bottle.js
+++ public/javascript/add-bottle.js
@@ -1,5 +1,27 @@
 async function addBottleHandler(event) {
   event.preventDefault();
 
-  
-} 
+  const whiskey_name = document.querySelector('#bottle-name').value.trim();
+  const price = document.querySelector('#bottle-price').value.trim();
+
+  if (whiskey_name && price) {
+    const response = await fetch('/api/whiskeys', {
+      method: 'post',
+      body: JSON.stringify({
+        whiskey_name,
+        price,
+        
+      }),
+      headers: { 'Content-Type': 'application/json'}
+    });
+
+    if(response.ok) {
+      document.location.reload();
+      return;
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document.querySelector('.new-btl-form').addEventListener('submit', addBottleHandler);
